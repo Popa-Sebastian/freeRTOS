be0dd082858d981521630f60c3e1e740bcde0655
Sebastian Popa <sebastian.popa@arobs.com>
2022-10-20 13:57:34 +0300

    Use static _create_msg to generate log text

--- 100User_Interface/App/Src/log.c
+++ 100User_Interface/App/Src/log.c
@@ -18,60 +18,52 @@
 /* Imports */
 extern QueueHandle_t _q_log;
 
-/* Function Declarations */
-void log_msg(char const* fmt, ...)
-{
-    static char textmsg[40];
-    static char message[80];
-    static char *msg = message;
+static char _textmsg[40];
+static char _message[80];
 
+static void _create_msg(char const* fmt, ...)
+{
     // Get time stamp
     TickType_t tick = xTaskGetTickCount();
     uint32_t ms = tick % 1000;
     uint32_t s = tick / 1000;
     uint32_t m = s / 60;
-    sprintf(message, "%02lu:%02lu.%03lu ", m % 60, s % 60, ms);
+    sprintf(_message, "%02lu:%02lu.%03lu ", m % 60, s % 60, ms);
 
     // Get text
     va_list arg;
     va_start(arg, fmt);
-    vsprintf((char*)textmsg, fmt, arg);
+    vsprintf((char*)_textmsg, fmt, arg);
     va_end(arg);
-    strcat(message, textmsg);
+    strcat(_message, _textmsg);
 
     // Append end line
-    strcat(message, "\r\n");
+    strcat(_message, "\r\n");
+}
+
+
+/* Function Declarations */
+void log_msg(char const* fmt, ...)
+{
+    static char *msg = _message;
+
+    va_list arg;
+    va_start(arg, fmt);
+    _create_msg(fmt, arg);
+    va_end(arg);
 
     // Send to print task
     xQueueSend(_q_log, &msg, portMAX_DELAY);
 }
 
-/* TODO: create a single local function that creates the text message and call that instead
- * of this lazy version
- */
-
 void log_msg_fromISR(char const* fmt, ...)
 {
-    static char textmsg[40];
-    static char message[80];
-    static char *msg = message;
+    static char *msg = _message;
 
-    // Get time stamp
-    TickType_t tick = xTaskGetTickCount();
-    uint32_t ms = tick % 1000;
-    uint32_t s = tick / 1000;
-    uint32_t m = s / 60;
-    sprintf(message, "%02lu:%02lu.%03lu ", m % 60, s % 60, ms);
-
-    // Get text
     va_list arg;
     va_start(arg, fmt);
-    vsprintf((char*)textmsg, fmt, arg);
+    _create_msg(fmt, arg);
     va_end(arg);
-    strcat(message, textmsg);
-
-    // Append end line
-    strcat(message, "\r\n");
 
     // Send to print task
     xQueueSendFromISR(_q_log, &msg, NULL);
