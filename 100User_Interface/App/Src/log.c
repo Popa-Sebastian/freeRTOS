0449a78133cb95810428da2bbc1ef7c628629e85
Sebastian Popa <sebastian.popa@arobs.com>
2022-10-20 13:40:41 +0300

    safe log_from_isr function [wip]

--- 100User_Interface/App/Src/log.c
+++ 100User_Interface/App/Src/log.c
@@ -46,4 +46,35 @@ void log_msg(char const* fmt, ...)
     xQueueSend(_q_log, &msg, portMAX_DELAY);
 }
 
+/* TODO: create a single local function that creates the text message and call that instead
+ * of this lazy version
+ */
+
+void log_msg_fromISR(char const* fmt, ...)
+{
+    static char textmsg[40];
+    static char message[80];
+    static char *msg = message;
+
+    // Get time stamp
+    TickType_t tick = xTaskGetTickCount();
+    uint32_t ms = tick % 1000;
+    uint32_t s = tick / 1000;
+    uint32_t m = s / 60;
+    sprintf(message, "%02lu:%02lu.%03lu ", m % 60, s % 60, ms);
+
+    // Get text
+    va_list arg;
+    va_start(arg, fmt);
+    vsprintf((char*)textmsg, fmt, arg);
+    va_end(arg);
+    strcat(message, textmsg);
+
+    // Append end line
+    strcat(message, "\r\n");
+
+    // Send to print task
+    xQueueSendFromISR(_q_log, &msg, NULL);
+}
+
 /* End of File */
