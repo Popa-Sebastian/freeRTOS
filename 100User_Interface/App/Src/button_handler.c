aa1eb6ccb5bcab9b2c06615ab4ffd272c5342c5f
Sebastian Popa <sebastian.popa@arobs.com>
2022-10-21 13:50:55 +0300

    Update menu cursor after button press with debouncing

--- 100User_Interface/App/Src/button_handler.c
+++ 100User_Interface/App/Src/button_handler.c
@@ -4,13 +4,26 @@
  *  Created on: Oct 20, 2022
  *      Author: sebastian.popa
  */
+#include <stdbool.h>
 
 #include "main.h"
+#include "button_handler.h"
+#include "lcd.h"
 #include "log.h"
 
-/* Static Variable Declarations */
+/* Imports */
+
+/* Static variables */
+static sButtonDebounce _upBtn = {0};
+static sButtonDebounce _downBtn = {0};
+static sButtonDebounce _enterBtn = {0};
 
 /* Static function prototypes */
+static void _button_press_log(uint16_t GPIO_Pin);
+static bool _is_button_debounced(uint16_t GPIO_Pin);
+static void _button_update_cursor_pos(uint16_t GPIO_Pin);
+
+/* Static Function Declaration */
 static void _button_press_log(uint16_t GPIO_Pin)
 {
     switch (GPIO_Pin) {
@@ -29,10 +42,72 @@ static void _button_press_log(uint16_t GPIO_Pin)
     }
 }
 
-/* Function Declarations */
+static bool _is_button_debounced(uint16_t GPIO_Pin)
+{
+    uint32_t tick = xTaskGetTickCount();
+    bool ret = false;
+
+    switch (GPIO_Pin)
+    {
+    case BTN_UP_Pin:
+    {
+        _upBtn.currentButtonPress = tick;
+        ret = ((_upBtn.currentButtonPress - _upBtn.lastButtonPress) > BUTTON_PRESS_DEBOUNCE_MS);
+        _upBtn.lastButtonPress = _upBtn.currentButtonPress;
+    }
+    break;
+    case BTN_DN_Pin:
+    {
+        _downBtn.currentButtonPress = tick;
+        ret = ((_downBtn.currentButtonPress - _downBtn.lastButtonPress) > BUTTON_PRESS_DEBOUNCE_MS);
+        _downBtn.lastButtonPress = _downBtn.currentButtonPress;
+    }
+    break;
+    case BTN_ENT_Pin:
+    {
+        _enterBtn.currentButtonPress = tick;
+        ret = ((_enterBtn.currentButtonPress - _enterBtn.lastButtonPress) > BUTTON_PRESS_DEBOUNCE_MS);
+        _enterBtn.lastButtonPress = _enterBtn.currentButtonPress;
+    }
+    break;
+    }
+
+    return ret;
+}
+
+static void _button_update_cursor_pos(uint16_t GPIO_Pin)
+{
+    sMenu *menu = LCD_GetMenuInstance();
+
+    switch (GPIO_Pin) {
+    case BTN_UP_Pin:
+        if (_is_button_debounced(GPIO_Pin))
+        {
+            menu->cursorPos = menu->cursorPos > 0 ? menu->cursorPos - 1 : 0;
+        }
+        break;
+    case BTN_DN_Pin:
+        if (_is_button_debounced(GPIO_Pin))
+        {
+            menu->cursorPos = (menu->cursorPos + 1) % LCD_MAX_OPTIONS;
+        }
+        break;
+    case BTN_ENT_Pin:
+        if (_is_button_debounced(GPIO_Pin))
+        {
+            __NOP();
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+/* Public Function Declarations */
 void button_press_isr_handler(uint16_t GPIO_Pin)
 {
     _button_press_log(GPIO_Pin);
+    _button_update_cursor_pos(GPIO_Pin);
 }
 
 /* End of File */
