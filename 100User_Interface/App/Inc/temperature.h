19f5f4392f283d6d0d714f55ac59b0b93c96daff
Sebastian Popa <73309171+Popa-Sebastian@users.noreply.github.com>
2022-11-01 17:42:39 +0200

    Task/temp unit (#23)
    * Temperature Unit Variable
    
    * Change measurement unit for temperature
    
    * Update format for temperature display

--- 100User_Interface/App/Inc/temperature.h
+++ 100User_Interface/App/Inc/temperature.h
@@ -15,12 +15,18 @@
 /* Defines */
 
 /* Enums */
+typedef enum
+{
+    TEMP_CELSIUS = 0x00,
+    TEMP_FARENHEIT = 0x01,
+} eTempUnit;
 
 /* Global variables */
 
 /* Public Function Declaration */
 void TEMP_Init(void);
 void TEMP_GetTemperature(void);
+void TEMP_SetTemperatureUnit(eTempUnit unit);
 
 #endif /* INC_TEMPERATURE_H_ */
 
--- 100User_Interface/App/Src/temperature.c
+++ 100User_Interface/App/Src/temperature.c
@@ -14,12 +14,14 @@
 
 #include "main.h"
 #include "temp_sensor.h"
+#include "temperature.h"
 #include "lcd.h"
 #include "log.h"
 
 extern SemaphoreHandle_t _mutex_display;
 
 /* Static Variable Declarations */
+static eTempUnit _tempUnit = TEMP_CELSIUS;
 
 /* Static function prototypes */
 
@@ -38,13 +40,31 @@ void TEMP_GetTemperature(void)
     xSemaphoreTake(_mutex_display, portMAX_DELAY);
 
     sMenu* menu = LCD_GetMenuInstance();
-    sprintf(menu->temperature, "%d.%dC", (uint16_t) (outputValue / 10), (uint8_t)(outputValue % 10));
+    if(_tempUnit == TEMP_CELSIUS)
+    {
+        sprintf(menu->temperature, "%02d.%dC", (uint16_t) (outputValue / 10), (uint8_t)(outputValue % 10));
+    }
+    else
+    {
+        uint16_t farenheitVal = (outputValue * 9 / 5) + 320;
+        sprintf(menu->temperature, "%03dF", (uint16_t) (farenheitVal / 10));
+    }
 
     strcpy(current_temperature, menu->temperature);
 
     xSemaphoreGive(_mutex_display);
 
     log_msg(current_temperature);
+}
+
+void TEMP_SetTemperatureUnit(eTempUnit unit)
+{
+    if (unit > TEMP_FARENHEIT)
+    {
+        return;
+    }
+
+    _tempUnit = unit;
 }
 
 /* End of File */
